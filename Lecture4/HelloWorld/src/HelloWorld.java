06d4d793c3ee151f97ad5355163aa5b82a35da6e
nicomancer-pelican <52576822+nicomancer-pelican@users.noreply.github.com>
2020-09-22 16:31:57 +0100

    Set up and first exercise

---
+++ Lecture4/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+ * File: HelloWorld.java
+ * ---------------------
+ * This programme displays "hello, world" on the screen.
+ * It is inspired by the first programme in Brian Kernighan and Dennis Ritchie's classic boock,
+ * The C Programming Language.
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
